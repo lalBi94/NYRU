41254e0f6462db48c8597ccb4fb745dad972a597
Bilal <bilal@boudjemline.fr>
2024-02-20 19:49:24 +0100

    $

--- src/components/YTBFrame/YTBFrame.jsx
+++ src/components/YTBFrame/YTBFrame.jsx
@@ -7,8 +7,7 @@ export default function YTBFrame({link}) {
             className="ytb-frame"
             title="YouTube video player"
             frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
+            allow="fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         ></iframe>
     );
 }
