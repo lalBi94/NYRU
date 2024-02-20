0e6c73ce029c0b50e41e1d212fea33cc2689ff80
Bilal <bilal@boudjemline.fr>
2024-02-20 20:03:56 +0100

    $

--- src/components/YTBFrame/YTBFrame.jsx
+++ src/components/YTBFrame/YTBFrame.jsx
@@ -1,13 +1,15 @@
+import React, { useState, useEffect } from 'react';
 import "./YTBFrame.scss";
 
-export default function YTBFrame({link}) {
+
+export default function YTBFrame({ link }) {
     return (
         <iframe
-            src={link}
-            className="ytb-frame"
-            title="YouTube video player"
-            frameborder="0"
-            allow="fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        ></iframe>
+                src={link}
+                className="ytb-frame"
+                title="YouTube video player"
+                frameborder="0"
+                allow="fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            ></iframe>
     );
 }
